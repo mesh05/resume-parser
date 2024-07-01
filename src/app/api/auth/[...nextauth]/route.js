b3d534348d08188f8104e6b83a4669ad2e4c2874
Prathamesh <pr.sawant.in@gmail.com>
2024-07-01 15:29:57 +0530

    auth complete

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,7 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import {authOptions} from "@/components/auth/authOptions";
 
-const handler = NextAuth({
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
---
+++ src/app/resume/create/page.jsx
@@ -0,0 +1,7 @@
+export default function ResumeCreatePage() {
+  return (
+    <div>
+      <h1>Create the Resume Here</h1>
+    </div>
+  );
+}
