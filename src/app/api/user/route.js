22c282fc40be217682fcd9a0a4ba432674f5083c
Prathamesh <pr.sawant.in@gmail.com>
2024-07-01 20:58:48 +0530

    db mid

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,7 +1,24 @@
-import { dbConnect } from "@/components/db/db";
+import connectToDatabase from "@/components/db/db";
+import User from "@/components/models/user";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const con = await dbConnect();
-  return new NextResponse("connected");
+async function handler(req, res) {
+  try {
+    await connectToDatabase();
+
+    // Perform your MongoDB operations here
+    const data = await User.db("resume")
+      .collection("User")
+      .findOne({ hi: "hello" })
+      .toArray();
+
+    return new NextResponse({ data }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new NextResponse({
+      error: "An error occurred while connecting to MongoDB",
+    });
+  }
 }
+
+export { handler as GET, handler as POST };
--- src/app/resume/create/page.jsx
+++ src/app/resume/create/page.jsx
@@ -1,37 +1,14 @@
 "use client";
-import { User } from "@/components/db/db";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ResumeCreatePage() {
-  const [user, changeUser] = useState(null);
-  axios.get("http://localhost:3000/api/user").then((res) => {
-    console.log(res.data);
-  });
-  //   const newUser = new User({
-  //     userId: 1,
-  //     userName: "mesh05",
-  //     name: "test",
-  //     email: "test",
-  //     github: "test",
-  //     linkedin: "test",
-  //     education: {
-  //       institute: "test",
-  //       degree: "test",
-  //       year: 2023,
-  //       GPA: 4.0,
-  //     },
-  //     experience: {
-  //       company: "test",
-  //       role: "test",
-  //       year: 2023,
-  //     },
-  //     skills: ["test"],
-  //     projects: [{ name: "test", description: "test" }],
-  //   });
-  //   newUser.save();
-  //   User.findOne({ userId: 1 }).then((user) => {
-  //     changeUser(user);
-  //   });
+  const [user, changeUser] = useState("null");
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/user").then((response) => {
+      changeUser(response.data);
+      console.log(response.data);
+    });
+  }, []);
   return <div>{user}</div>;
 }
