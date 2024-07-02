53220f1efcad5f69291ccdab5b35209dca72ab1d
Prathamesh <pr.sawant.in@gmail.com>
2024-07-02 08:53:21 +0530

    dkjf

--- src/app/resume/create/page.jsx
+++ src/app/resume/create/page.jsx
@@ -1,9 +1,9 @@
 "use client";
 import axios from "axios";
+import FileSaver from "file-saver";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { cookies, headers } from "next/headers";
 
 export default function ResumeCreatePage() {
   const [user, changeUser] = useState("null");
@@ -27,22 +27,27 @@ export default function ResumeCreatePage() {
       />
       <button
         onClick={() => {
-          axios
-            .post("http://localhost:8000/details", {
-              headers:{
-                "X-CSRFToken":cookies().get('next-auth.csrf-token')?.value.split('|')[0]
-              },
+          axios({
+            method: "post",
+            url: "http://localhost:8000/details",
+            data: {
               link: link,
               user: user,
-            })
-            .then((response) => {
-              const url = window.URL.createObjectURL(new Blob([response.data]));
-              const link = document.createElement("a");
-              link.href = url;
-              link.setAttribute("download", "resume.docx"); //or any other extension
-              document.body.appendChild(link);
-              link.click();
-            });
+            },
+            responseType: "arraybuffer", // Ensure Axios treats the response as a blob
+          }).then((response) => {
+            const url = window.URL.createObjectURL(
+              new Blob([response.data], {
+                type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+              })
+            );
+            const downloadLink = document.createElement("a");
+            downloadLink.href = url;
+            downloadLink.setAttribute("download", "resume1123.docx"); //or any other extension
+            document.body.appendChild(downloadLink);
+            downloadLink.click();
+            document.body.removeChild(downloadLink);
+          });
         }}
         className="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
